120aca501096870316a90471f26bbdf0fe151d4a
Zekwan ShekYusuf Ahmed <zekuwany@gmail.com>
2025-12-12 22:01:30 +0300

    Update ClippedComposite.js

--- examples/javascripts/ClippedComposite.js
+++ examples/javascripts/ClippedComposite.js
@@ -8,13 +8,13 @@ var collection = ee.ImageCollection('LANDSAT/LE07/C02/T1')
 var median = collection.median();
 
 // Load a table of state boundaries and filter.
-var Park_Catchment = ee.FeatureCollection('projects/ee-zekuwany/assets/SHP/Park_Catchment');
+var Park_Catchment = ee.FeatureCollection('projects/ee-zekuwany/assets/SHP/Gara_Mullata_Park');
 var fc = Park_Catchment.geometry();
 
 // Clip to the output image to the Nevada and Arizona state boundaries.
 var clipped = median.clipToCollection(fc);
 
 // Display the result.
-Map.setCenter(41.80, 8.59, 5);
+Map.setCenter(9.2432,41.7556, 12);
 var visParams = {bands: ['B3', 'B2', 'B1'], gain: [1.4, 1.4, 1.1]};
 Map.addLayer(clipped, visParams, 'clipped composite');
